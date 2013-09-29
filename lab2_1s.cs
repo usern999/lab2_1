e05700d9b373a2592cb92fe2d3e171d2e96b0a97
usern <999998@inbox.ru>
2013-09-29 23:24:59 +0400

    lab2_1

---
+++ lab2_1s.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lab2_0;
+
+
+namespace lab2_1
+{
+    class lab2_1s
+    {
+        static void Main(string[] args)
+        {
+            
+            Consolelogger Clog = new Consolelogger();
+            Severity s = Severity.Error;
+            Clog.Log(s, "Error");
+            Clog.Indent();
+            s = Severity.Warning;
+            Clog.Log(s, "Warning");
+            Clog.Unindent();
+            s = Severity.Info;
+            Clog.Log(s, "Info");
+            Console.Read();
+        }
+    }
+}
